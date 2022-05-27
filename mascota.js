02cedf79deaf861a01d3c876f567eaae8da2ce39
AnyFernandez <any.atrevete@gmail.com>
2022-05-27 20:55:22 -0300

    sume mascota

--- mascota.js
+++ mascota.js
@@ -1,6 +1,13 @@
-const mascotas={
+const mascota={
     nombre:"negra",
     raza:"mestiza",
-    edad:"1 año"
+    edad:"1 años"
     }
-    console.log(mascotas);
+const mascota2={
+    nombre:"blanca",
+    raza:"siames",
+    edad:"2 años"
+}    
+    console.log(mascota);
+    console.log(mascota2);
+
